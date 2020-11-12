348471e1a95eff45f8289845e60a56d11f1b2462
Honshu11 <egonza67@yahoo.com>
2020-11-11 18:07:38 -0800

    adding html/css to ToDo App

---
+++ ToDoApp/ToDo.js
@@ -0,0 +1,10 @@
+// Javascript ToDo list logic
+
+const clear = document.querySelector(".clearBtn"); // this method targets the class
+const date = document.getElementById("date"); // this method targets the Id 
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+let list = [];
+let id = 0;
+
